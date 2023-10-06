0aaaa5bf5730d1ed5fdc96815e3f00af3d1e60b5
Dani Vansia <41837012+vansia43@users.noreply.github.com>
2023-10-06 10:50:27 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,6 +92,35 @@ const config = {
         disableSwitch: true,
         respectPrefersColorScheme: false,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'FETJSHIQX2',
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'db0617cc49d4d8f18b6358e96617261d',
+  
+        indexName: 'vantage',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //  from: '/docs/', // or as RegExp: /\/docs\//
+        //  to: '/',
+        //},
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
       navbar: {
         logo: {
           alt: "Logo",
