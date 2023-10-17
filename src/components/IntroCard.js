9ab1e0fa4a4b5518198fd7720369c606efe3ea67
Everett Berry <everett@vantage.sh>
2023-10-17 19:13:53 -0400

    don't open cards in new tab

--- src/components/IntroCard.js
+++ src/components/IntroCard.js
@@ -1,21 +1,21 @@
 // IntroCard.js
-import React from 'react';
-import '../css/card.css'; 
+import React from "react";
+import "../css/card.css";
 
 const IntroCard = ({ title, content, image, link, alt }) => (
   <div className="card">
-    <a href={link} className="card-link" target="_blank" rel="noopener noreferrer">
+    <a href={link} className="card-link">
       <div className="card-image">
         <img src={image} alt={alt} />
       </div>
-        <div className="card-title">
-          <h2>{title}</h2>
-        </div>
-        <div className="card-content">
-          <p>{content}</p>
-        </div>
-      </a>
-    </div>
+      <div className="card-title">
+        <h2>{title}</h2>
+      </div>
+      <div className="card-content">
+        <p>{content}</p>
+      </div>
+    </a>
+  </div>
 );
 
 export default IntroCard;
