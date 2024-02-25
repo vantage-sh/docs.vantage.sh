967e3c63946d93002d5c6a5570db02f56e9bfd01
Dani Vansia <41837012+vansia43@users.noreply.github.com>
2024-02-24 23:33:38 -0500

    Add FCR docs

--- sidebars.js
+++ sidebars.js
@@ -172,6 +172,7 @@ module.exports = {
         "budgets",
         "per_unit_costs",
         "issues",
+        "financial_commitment_reports",
         "savings_planner",
         "cost_recommendations",
         ],
